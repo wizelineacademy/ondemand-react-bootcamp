4c50c1983e0a7f25d549a9b2b86d95eb29a20619
Fernando Reina Lopez <57535554+FernandoWL@users.noreply.github.com>
2022-06-02 16:47:05 -0500

    Css updated

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,18 +1,26 @@
-import React from "react";
+import {useState} from "react";
 import "./header.css";
 import logo from "../../../assets/images/logo.png";
 import cart from "../../../assets/images/shopping_cart.png";
+import cart_active from "../../../assets/images/shopping_cart_active.png";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isHover, setIsHover] = useState(false);
+  const handlerOnMouseEnter = () => {
+    setIsHover(prevIsHover => (true));
+  }
+  const handlerOnMouseLeave = () =>{
+    setIsHover(prevIsHover => (false));
+  }
   return (
     <div className="header">
       <div className="header-logo">
-        <NavLink to="/"><img src={logo} alt="logo"/></NavLink>
+        <NavLink to="/"><img src={logo} alt="logo"style={{height:120}}/></NavLink>
       </div>
       <div className="header-right">
         <input type="text" placeholder="Search item"></input>
-        <img src={cart} alt="cart" style={{height:50}}/>
+        <img src={isHover ? cart_active : cart } alt="cart" style={{height:60}} onMouseEnter={handlerOnMouseEnter} onMouseLeave={handlerOnMouseLeave}/>
       </div>
     </div>
   );
