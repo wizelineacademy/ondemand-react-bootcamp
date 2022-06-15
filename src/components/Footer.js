f3ae1bfdcfd147cae5a5b372db5c75bb4523fbcc
guzmangarcia <guzman.garcia@wizeline.com>
2022-06-15 18:19:23 +0200

    removes bootstrap adds extra elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,77 +1,13 @@
 import styles from './Footer.module.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow
-} from 'mdb-react-ui-kit';
+export default function Footer() 
+{
 
-
-export default function Footer() {
     return (
-      <MDBFooter bgColor='light' className={ `text-center text-lg-left ${styles.textfooter }` } >
-        <MDBContainer className='p-4'>
-          <MDBRow>
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase'>Links</h5>
-  
-              <ul className='list-unstyled mb-0'>
-                <li>
-                  <a href='#!' className='text-dark'>
-                    Link 1
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-  
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase mb-0'>Links</h5>
-  
-              <ul className='list-unstyled'>
-                <li>
-                  <a href='#!' className='text-dark'>
-                    Link 1
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-  
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase'>Links</h5>
-  
-              <ul className='list-unstyled mb-0'>
-                <li>
-                  <a href='#!' className='text-dark'>
-                    Link 1
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-  
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase mb-0'>Links</h5>
-  
-              <ul className='list-unstyled'>
-                <li>
-                  <a href='#!' className='text-dark'>
-                    Link 1
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-  
-        <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          &copy; {new Date().getFullYear()} Copyright:{' '}
-          <a className='text-dark' href='https://www.wizeline.com/'>
-            www.wizeline.com
-          </a>
-        </div>
-      </MDBFooter>
+
+        <p>Ecommerce created during Wizeline’s Academy React Bootcamp</p>
+
     );
-  }
-  
+
+
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,39 @@
 import styles from './Header.module.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../media/logo.png'; 
+import shoppingCart from '../media/shopping-cart.png'; 
+import styled from 'styled-components';
 
-export default function Header ()
-{
+
+  
+
+
+const  SearchEngine = () => {
+
     return (
-        <p className={`${styles.textHeader }`}>Header</p>
-
+        
+        <input className={styles.seach}/>
     ) ;
 
-}
+};
+
+
+
+const Header  =()=> {
+
+    return (
+        
+        <table className={styles.table}>
+            <tr>
+                <th>Authentic Authentic Authentic!</th>
+                <th><img src={logo} alt={'Authentic Authentic Authentic' }  width={100} height={100}/></th>
+              
+                <th><SearchEngine/ ></th>  
+                <th><img src={shoppingCart} alt={'check your purchase' }  width={50} height={50}/></th>
+            </tr>
+        </table>
+    ) ;
+
+};
+
+
+export default Header;
