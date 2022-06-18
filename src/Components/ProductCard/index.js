b8f9305f0ddac0d9a2e41234fe7ca46e5fe4b60d
leo_mcis <leo@thebrandindustry.com>
2022-06-18 13:11:56 -0500

    Product info

--- src/Components/ProductCard/index.js
+++ src/Components/ProductCard/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './styles.scss';
 
 const ProductCard = (props) => {
-  const { name, mainimage } = props;
+  const { name, mainimage, category, price } = props;
+
+  function FormatPrice(price) {
+    const formattedPrice = price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // 12,345.67
+    return '$' + formattedPrice;
+  }
+
   return (
     <div className="ProductCard">
       <img src={mainimage.url} alt={name} />
       <p>{name}</p>
+      <span className="product__category">{category.slug}</span>
+      <span className="product__price">{FormatPrice(price)}</span>
     </div>
   );
 };
