32b73480d020fd44361ace5b7024ff3259202748
guzmangarcia <guzman.garcia@wizeline.com>
2022-07-06 11:45:45 +0200

    refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import styles from './Home.module.scss'
 //import useProductCategories from  '../utils/hooks/useProductCategories';
 
 
-
 import useFeaturedBanners  from '../utils/hooks-mooks/useFeaturedBanners';
 import useFeaturedProducts from '../utils/hooks-mooks/useFeaturedProducts';
 import useProductCategories from  '../utils/hooks-mooks/useProductCategories';
