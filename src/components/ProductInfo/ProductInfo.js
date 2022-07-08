f79bbfa440f72253cc09e22e241fdcb2f0a1a1a8
guzmangarcia <guzman.garcia@wizeline.com>
2022-07-08 09:49:01 +0200

    adds categories data load

---
+++ src/components/ProductInfo/ProductInfo.js
@@ -0,0 +1,63 @@
+import { useState, useEfect } from "react";
+
+export default function ProductInfo( props)
+{
+    return(
+        <div style={{display: 'inline-block'}}>
+
+            sadasdsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            <br/>
+            dadsada
+            
+        </div>
+
+    );
+
+
+
+}
