6fa1b25c9a166268009a1a05fd30547d787d464b
guzmangarcia <guzman.garcia@wizeline.com>
2022-07-20 13:05:22 +0200

    search page

--- src/components/ProductDescription/ProductDescription.js
+++ src/components/ProductDescription/ProductDescription.js
@@ -1,17 +1,17 @@
 export default function ProductDescription({ name, price, sku, categoryName, tags, description, specs }) {
     
     
-    console.log(name, price, sku, categoryName, tags, description, specs)
+   
 
     return (
         <div>
-            {<p>Name: {name}</p>}
-            {<p>Price: {price}</p>}
-            {<p>SKU: {sku}</p>}
-            {<p>Category: {categoryName}</p>}
-            <p>Tags: {tags.map((tag, id) => { return (<span key={id}>{tag} </span>) })}</p>
-            {<p>Description: <br /> {description}</p>}
-            <div>Qty: <input type={'number'} value={1} />  <button >Add to cart</button></div>
+            <p><label>Name: {name}</label></p>
+            <p><label>Price: {price}</label></p>
+            <p><label>SKU: {sku}</label></p>
+            <p><label>Category: {categoryName}</label></p>
+            <p><label>Tags: {tags.map((tag, id) => { return (<label key={id}>{tag} </label>) })}</label></p>
+            <p><label>Description: <br /> {description}</label></p>
+            <div>Qty: <input type={'number'}  />  <button >Add to cart</button></div>
             <div><p>Specs:</p>
                 <ul>
                     {specs.map((spec, index) => { return (<li key={index}>{spec.spec_name}: {spec.spec_value}</li>) })}
